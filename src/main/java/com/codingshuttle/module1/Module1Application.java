eb8978b7657b20f569fd3da7e3824e43698ebca4
chandan.yadav <cy75945@gmail.com>
2025-12-26 08:31:58 +0530

    conditional on propery code added

--- src/main/java/com/codingshuttle/module1/Module1Application.java
+++ src/main/java/com/codingshuttle/module1/Module1Application.java
@@ -1,6 +1,7 @@
 package com.codingshuttle.module1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Module1Application implements CommandLineRunner {
     @Autowired
     PaymentService paymentService;
+
+//    Autowired shoud not be used in production
+//    @Autowired
+//    NotificationService notificationService;
+
+
+//    will use constructor method DI
+        final NotificationService notificationService;
+//        We have define the qualifier now we need to set the qualifier here alos
+    public Module1Application(NotificationService notificationService){
+        this.notificationService=notificationService;
+    }
+
+    //now with the help of constructor method of di we can not change the notificaiton service
+//    ex: NotificationService=null;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Module1Application.class, args);
 	}
@@ -17,5 +34,6 @@ public class Module1Application implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         paymentService.pay();
+        notificationService.send("sending");
     }
 }
